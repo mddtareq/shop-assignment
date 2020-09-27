47ee27761704bbc8ec2e4b6b2e7bac36de7801b6
Mohammad Tareq <mdtareq320@gmail.com>
2020-09-27 16:02:05 +0600

    initially done

--- js/custom/account.js
+++ js/custom/account.js
@@ -13,16 +13,28 @@ secondC.addEventListener('click', function () {
     items--;
     itemsF()
 })
+document.body.addEventListener('click', function (event) {
+    const firstC = document.getElementById('firstB');
+    const secondC = document.getElementById('secondB');
+    const cartDisplay = document.getElementById("see-cart");
+    const cart = document.getElementById('cart-button');
+    const secondItemsC = document.getElementById('see-second-cart');
+    const firstItemsC = document.getElementById('see-first-cart');
+    const secondInfo= document.getElementById('mini-info-2');
+    const firstInfo = document.getElementById('mini-info-1');
+    if(event.target===cart){
+        cartDisplay.style.display = 'block';
+    }
+    if(event.target!==cart){
+        
+        if(event.target!==cartDisplay&&
+            event.target!==firstC&&event.target!==secondC&&
+            event.target.parentNode!==secondItemsC&&event.target.parentNode!==firstItemsC&&
+            event.target.parentNode!==secondInfo&&event.target.parentNode!==firstInfo){
+            cartDisplay.style.display = 'none';
+        }
+    }
 
-const cart = document.getElementById('cart-button');
-cart.addEventListener('mouseover', function () {
-    const cartDisplay= document.getElementById("see-cart");
-    cartDisplay.style.display = 'block';
-})
-const cartHide = document.getElementById('hide-cart');
-cartHide.addEventListener('click', function () {
-    const cartDisplay= document.getElementById("see-cart");
-    cartDisplay.style.display = 'none';
 })
 function itemsF() {
     if (items<0) {
--- js/custom/index.js
+++ js/custom/index.js
@@ -1,4 +1,4 @@
-let items=1;
+let items = 1;
 const firstC = document.getElementById('firstB');
 firstC.addEventListener('click', function () {
     const firstItemsC = document.getElementById('see-first-cart');
@@ -13,23 +13,35 @@ secondC.addEventListener('click', function () {
     items--;
     itemsF()
 })
+document.body.addEventListener('click', function (event) {
+    const firstC = document.getElementById('firstB');
+    const secondC = document.getElementById('secondB');
+    const cartDisplay = document.getElementById("see-cart");
+    const cart = document.getElementById('cart-button');
+    const secondItemsC = document.getElementById('see-second-cart');
+    const firstItemsC = document.getElementById('see-first-cart');
+    const secondInfo= document.getElementById('mini-info-2');
+    const firstInfo = document.getElementById('mini-info-1');
+    if(event.target===cart){
+        cartDisplay.style.display = 'block';
+    }
+    if(event.target!==cart){
+        
+        if(event.target!==cartDisplay&&
+            event.target!==firstC&&event.target!==secondC&&
+            event.target.parentNode!==secondItemsC&&event.target.parentNode!==firstItemsC&&
+            event.target.parentNode!==secondInfo&&event.target.parentNode!==firstInfo){
+            cartDisplay.style.display = 'none';
+        }
+    }
 
-const cart = document.getElementById('cart-button');
-cart.addEventListener('mouseover', function () {
-    const cartDisplay= document.getElementById("see-cart");
-    cartDisplay.style.display = 'block';
-})
-const cartHide = document.getElementById('hide-cart');
-cartHide.addEventListener('click', function () {
-    const cartDisplay= document.getElementById("see-cart");
-    cartDisplay.style.display = 'none';
 })
 function itemsF() {
-    if (items<0) {
-        document.getElementById("nothing").style.display='block';
+    if (items < 0) {
+        document.getElementById("nothing").style.display = 'block';
     }
     itemNumber();
 }
-function itemNumber(){
-        document.getElementById("cart-size-number").innerText=items+1;
+function itemNumber() {
+    document.getElementById("cart-size-number").innerText = items + 1;
 }
